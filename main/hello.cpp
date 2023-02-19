afbc57dd44db9fb9e9803bd2aff6959c7c1f6522
Sanath-07 <79130625+Sanath-07@users.noreply.github.com>
2023-02-19 20:07:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins, this is PES1UG20CS372\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
